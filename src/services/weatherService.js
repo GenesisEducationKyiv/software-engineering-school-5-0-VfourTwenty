1962b60698d1d8780e0c9cfec53fd9eae6a90ef3
VfourTwenty <jatenee@gmail.com>
2025-06-24 12:41:34 +0300

    refactored weatherService

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,8 +1,33 @@
-const providerState  = require('../providers/state.js');
+/**
+ * @typedef {Object} WeatherProvider
+ * @property {function(string): Promise<any>} fetchWeather
+ */
 
-function fetchWeather(city)
-{
-    return providerState.activeWeatherProvider.fetchWeather(city);
+const providerState = require('../providers/state.js');
+
+class WeatherService {
+    /**
+     * The active weather provider (can be swapped for testing or runtime changes)
+     * @type {WeatherProvider}
+     */
+    static provider = providerState.activeWeatherProvider;
+
+    /**
+     * Set the active weather provider
+     * @param {WeatherProvider} provider
+     */
+    static setProvider(provider) {
+        WeatherService.provider = provider;
+    }
+
+    /**
+     * Fetch weather using the current provider
+     * @param {string} city
+     * @returns {Promise<any>}
+     */
+    static async fetchWeather(city) {
+        return WeatherService.provider.fetchWeather(city);
+    }
 }
 
-module.exports = { fetchWeather }
+module.exports = WeatherService;
