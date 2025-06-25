75c1206fb0e4262369c9473ee7935edd990870fd
VfourTwenty <jatenee@gmail.com>
2025-06-25 15:48:23 +0300

    moved imports to the top of the file

--- src/app.js
+++ src/app.js
@@ -4,19 +4,19 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const createError = require('http-errors');
 
-const app = express();
+const weatherRouter = require('./routes/weather');
+const subscriptionRouter = require('./routes/subscription');
+const publicRouter = require('./routes/public')
 
 const cronMain = require('./cron/main');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-const weatherRouter = require('./routes/weather');
-const subscriptionRouter = require('./routes/subscription');
-const publicRouter = require('./routes/public')
 
 // Mount routes
 app.use('/api', weatherRouter);
