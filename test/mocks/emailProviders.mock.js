9cea1ad256c9282bc6372f0d24b57e5379cfd038
VfourTwenty <jatenee@gmail.com>
2025-07-15 13:47:18 +0300

    added unit tests for email provider manager

---
+++ test/mocks/emailProviders.mock.js
@@ -0,0 +1,33 @@
+const IEmailProvider = require('../../src/providers/email-providers/emailProviderInterface');
+
+class EmailProviderMock1 extends IEmailProvider
+{
+    async sendEmail(to, subject, body)
+    {
+        if (to === 'email1@mail.com')
+        {
+            return { success: true }
+        }
+        else
+        {
+            return {success: false, error: new Error('Email provider 1 failed')}
+        }
+    }
+}
+
+class EmailProviderMock2 extends IEmailProvider
+{
+    async sendEmail(to, subject, body)
+    {
+        if (to === 'email2@mail.com')
+        {
+            return { success: true }
+        }
+        else
+        {
+            return {success: false, error: new Error('Email provider 2 failed')}
+        }
+    }
+}
+
+module.exports = { EmailProviderMock1, EmailProviderMock2 }
---
+++ test/unit/emailProviderManager.test.js
@@ -0,0 +1,32 @@
+const { expect } = require('chai');
+const EmailProviderManager = require('../../src/providers/email-providers/emailProviderManager');
+const { EmailProviderMock1, EmailProviderMock2 } = require('../mocks/emailProviders.mock');
+
+const mockedEmailProviders = [new EmailProviderMock1(), new EmailProviderMock2()];
+const emailProviderManager = new EmailProviderManager(mockedEmailProviders);
+
+// will add logging testing when logging has been implemented
+
+describe('EmailProviderManager Unit Tests', () => {
+
+    it('should return success when the first available provider succeeds', async () => {
+        const response = await emailProviderManager.sendEmail('email1@mail.com', 'hello', 'hello')
+        expect(response).to.deep.eq({ success: true });
+    })
+
+    it('should delegate to the next provider and return success when the first available provider fails', async () => {
+        const response = await emailProviderManager.sendEmail('email2@mail.com', 'hello', 'hello')
+        expect(response).to.deep.eq({ success: true });
+    })
+
+    it('should return null if all available providers fail', async () => {
+        const response = await emailProviderManager.sendEmail('email3@mail.com', 'hello', 'hello')
+        expect(response).to.be.null;
+    })
+
+    it('should return null if provider list is empty', async () => {
+        const emptyEmailProviderManager = new EmailProviderManager([]);
+        const response = await emptyEmailProviderManager.sendEmail('email3@mail.com', 'hello', 'hello')
+        expect(response).to.be.null;
+    })
+});
