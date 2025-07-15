fecbbd42eef998d41bd7100e998aab20a761bbf3
VfourTwenty <jatenee@gmail.com>
2025-07-15 13:27:17 +0300

    added unit tests for weather provider manager

---
+++ test/mocks/weatherProviders.mock.js
@@ -0,0 +1,39 @@
+const IWeatherProvider = require('../../src/providers/weather-providers/weatherProviderInterface');
+
+class WeatherProviderMock1 extends  IWeatherProvider
+{
+    async fetchWeather(city)
+    {
+        if (["Kyiv", "Lviv"].includes(city)) {
+            return {
+                temperature: 22,
+                humidity: 60,
+                description: "Clear sky"
+            };
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
+
+class WeatherProviderMock2 extends  IWeatherProvider
+{
+    async fetchWeather(city)
+    {
+        if (["Odesa", "Dnipro"].includes(city)) {
+            return {
+                temperature: 22,
+                humidity: 60,
+                description: "Clear sky"
+            };
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
+
+module.exports = { WeatherProviderMock1, WeatherProviderMock2 }
---
+++ test/unit/weatherProviderManager.test.js
@@ -0,0 +1,40 @@
+const { expect } = require('chai');
+const WeatherProviderManager = require('../../src/providers/weather-providers/weatherProviderManager');
+const { WeatherProviderMock1, WeatherProviderMock2 } = require('../mocks/weatherProviders.mock');
+
+const mockedWeatherProviders = [new WeatherProviderMock1(), new WeatherProviderMock2()];
+const weatherProviderManager = new WeatherProviderManager(mockedWeatherProviders);
+
+// will add logging testing when logging has been implemented
+
+describe('WeatherProviderManager Unit Tests', () => {
+
+    it('should return weather data if the first available provider can fetch it', async () => {
+        const data = await weatherProviderManager.fetchWeather('Kyiv');
+        expect(data).to.deep.equal({
+            temperature: 22,
+            humidity: 60,
+            description: "Clear sky"
+        });
+    });
+
+    it('should delegate to the next provider and return weather data if the first provider fails to fetch', async () => {
+        const data = await weatherProviderManager.fetchWeather('Odesa');
+        expect(data).to.deep.equal({
+            temperature: 22,
+            humidity: 60,
+            description: "Clear sky"
+        });
+    });
+
+    it('should return null if all available providers fail', async () => {
+        const data = await weatherProviderManager.fetchWeather('gfhhhh');
+        expect(data).to.be.null;
+    });
+
+    it('should return null if provider list is empty', async () => {
+        const emptyWeatherProviderManager = new WeatherProviderManager([])
+        const data = await emptyWeatherProviderManager.fetchWeather('Kyiv');
+        expect(data).to.be.null;
+    });
+});
