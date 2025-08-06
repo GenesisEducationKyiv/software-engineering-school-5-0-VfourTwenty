569f92af67527b1883843f5416773bb6016805ff
VfourTwenty <jatenee@gmail.com>
2025-08-06 14:06:02 +0300

    updated error hadnling in subscription service

--- microservices/apps/subscription/src/common/utils/clientErrors.js
+++ microservices/apps/subscription/src/common/utils/clientErrors.js
@@ -1,28 +1,28 @@
 const errorMap = {
     // subscription
-    'MISSING REQUIRED FIELDS': { status: 400 },
-    'INVALID EMAIL FORMAT': { status: 400 },
-    'INVALID CITY': {status: 400 },
-    'INVALID FREQUENCY': {status: 400},
-    'DUPLICATE': {status: 409},
-    'FAILED TO CREATE SUBSCRIPTION': {status: 400},
+    'MISSING REQUIRED FIELDS': 400,
+    'INVALID EMAIL FORMAT': 400,
+    'INVALID CITY': 400,
+    'INVALID FREQUENCY': 400,
+    'DUPLICATE': 409,
+    'FAILED TO CREATE SUBSCRIPTION': 400,
     // confirmation
-    'ALREADY CONFIRMED': {status: 400},
-    'CONFIRMATION FAILED': {status: 400},
+    'ALREADY CONFIRMED': 400,
+    'CONFIRMATION FAILED': 400,
     // unsubscribe
-    'FAILED TO DELETE SUBSCRIPTION': {status: 400},
+    'FAILED TO DELETE SUBSCRIPTION': 400,
     // confirm and unsubscribe
-    'SUBSCRIPTION NOT FOUND': {status: 400},
-    'TOKEN NOT FOUND': {status: 404},
-    'INVALID TOKEN': {status: 400}
+    'SUBSCRIPTION NOT FOUND': 400,
+    'TOKEN NOT FOUND': 404,
+    'INVALID TOKEN': 400
 };
 
 function handleError(err, res, map = errorMap)
 {
-    const mapped = map[err];
-    if (mapped) 
+    const status= map[err];
+    if (status)
     {
-        return res.status(mapped.status).json({ error: err });
+        return res.status(status).json({ error: err });
     }
     else 
     {
