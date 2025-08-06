4c2d96eec90f20dd8896290212adf6ab458c727a
VfourTwenty <jatenee@gmail.com>
2025-08-06 21:06:52 +0300

    updated start script

--- microservices/scripts/start-all-apps.sh
+++ microservices/scripts/start-all-apps.sh
@@ -1,1 +1,3 @@
-./create-db-if-needed.sh && docker compose up --build frontend backend subscription db weather email redis
+./create-db-if-needed.sh && \
+docker compose up --build -d rabbitmq &&
+docker compose up --build frontend backend subscription db weather email notification redis
