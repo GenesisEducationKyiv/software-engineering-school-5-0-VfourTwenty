a5e54599ac04346219a20148bf8259a730a7c0c7
VfourTwenty <jatenee@gmail.com>
2025-08-08 09:43:30 +0300

    modified script to start redis in detached mode

--- microservices/scripts/start-all-apps.sh
+++ microservices/scripts/start-all-apps.sh
@@ -1,3 +1,3 @@
 ./create-db-if-needed.sh && \
-docker compose up --build -d rabbitmq &&
-docker compose up --build frontend backend subscription db weather email notification redis
+docker compose up --build -d redis rabbitmq &&
+docker compose up --build frontend backend subscription db weather email notification
