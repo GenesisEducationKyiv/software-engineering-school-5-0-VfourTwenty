10f9bc026ea4a724cdbee933b9aaf8e4cf776480
VfourTwenty <jatenee@gmail.com>
2025-08-09 08:47:15 +0300

    removed a deprecated comment

--- microservices/scripts/unit-tests-all.sh
+++ microservices/scripts/unit-tests-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-# will add weather later
+
 for service in backend weather subscription email; do
   echo "Running unit tests in $service..."
   (cd ../apps/$service && npm run test:unit)
