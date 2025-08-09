38a978537ae08fe28c42261d4612fe34d8c71374
VfourTwenty <jatenee@gmail.com>
2025-08-09 15:29:49 +0300

    updated logging in subscription service

--- microservices/apps/subscription/src/application/subscriptionService.js
+++ microservices/apps/subscription/src/application/subscriptionService.js
@@ -4,9 +4,10 @@ const metricsKeys = require('../common/metrics/metricsKeys');
 
 class SubscriptionService
 {
-    constructor(subscriptionRepo, metricsProvider)
+    constructor(subscriptionRepo, logger, metricsProvider)
     {
         this.subscriptionRepo = subscriptionRepo;
+        this.log = logger.for('SubscriptionService');
         this.metricsProvider = metricsProvider;
     }
 
@@ -15,6 +16,7 @@ class SubscriptionService
         const duplicateCheckResult = await this.subscriptionRepo.findSub({ email: email, city: city, frequency: frequency });
         if (duplicateCheckResult.success)
         {
+            this.log.warn('Duplicate subscription attempt', duplicateCheckResult.data);
             return new Result(false, 'DUPLICATE');
         }
 
@@ -22,9 +24,11 @@ class SubscriptionService
         const createResult = await this.subscriptionRepo.createSub({ email, city, frequency, confirmed: false, token });
         if (!createResult.success)
         {
+            this.log.error('Failed to create subscription', createResult.err);
             return new Result(false, 'FAILED TO CREATE SUBSCRIPTION');
         }
 
+        this.log.info('Created new subscription', { email, city, frequency });
         this.metricsProvider.incrementCounter(metricsKeys.SUBSCRIPTIONS_CREATED_TOTAL);
         return new Result(true, null, { token: token });
     }
@@ -35,20 +39,24 @@ class SubscriptionService
         const sub = result.data;
         if (!sub)
         {
+            this.log.warn('Subscription not found', result.err);
             return new Result(false, 'TOKEN NOT FOUND');
         }
 
         if (sub.confirmed)
         {
+            this.log.warn('Duplicate confirmation attempt', sub);
             return new Result(false, 'ALREADY CONFIRMED');
         }
 
         const confirmResult = await this.subscriptionRepo.confirmSub(token);
         if (!confirmResult.success)
         {
+            this.log.error('Confirmation failed', confirmResult.err);
             return new Result(false, 'CONFIRMATION FAILED');
         }
 
+        this.log.info('Subscription confirmed', sub);
         this.metricsProvider.incrementCounter(metricsKeys.SUBSCRIPTIONS_CONFIRMED_TOTAL);
         return new Result(true, null, { city: sub.city, frequency: sub.frequency });
     }
@@ -59,32 +67,41 @@ class SubscriptionService
         const sub = result.data;
         if (!sub)
         {
+            this.log.warn('Subscription not found', result.err);
             return new Result(false, 'TOKEN NOT FOUND');
         }
 
         const deleteResult = await this.subscriptionRepo.deleteSub(token);
         if (!deleteResult.success)
         {
+            this.log.error('Failed to delete subscription', deleteResult.err);
             return new Result(false, 'FAILED TO DELETE SUBSCRIPTION');
         }
 
+        this.log.info('Subscription deleted', sub);
         this.metricsProvider.incrementCounter(metricsKeys.SUBSCRIPTIONS_CANCELED_TOTAL);
         return new Result(true, null, { email: sub.email, city: sub.city });
     }
 
     async findSub(params) 
     {
-        return await this.subscriptionRepo.findSub(params);
+        const result = await this.subscriptionRepo.findSub(params);
+        this.log.debug('findSub result', result);
+        return result;
     }
 
     async findAllSubs(params)
     {
-        return await this.subscriptionRepo.findAllSubs(params);
+        const result = await this.subscriptionRepo.findAllSubs(params);
+        this.log.debug('find all subs success and error', { success: result.success, error: result.err });
+        return result;
     }
 
     async clear()
     {
-        return await this.subscriptionRepo.clear();
+        const result = await this.subscriptionRepo.clear();
+        this.log.debug('Clear subscriptions called. Result: ', result);
+        return result;
     }
 }
 
