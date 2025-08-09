e331b7bb168df4e64f65601c35804b5ea97d714d
VfourTwenty <jatenee@gmail.com>
2025-08-09 12:32:29 +0300

    added new metrics to weather service

--- microservices/apps/weather/src/common/metrics/metricsKeys.js
+++ microservices/apps/weather/src/common/metrics/metricsKeys.js
@@ -1,6 +1,12 @@
 const METRIC_KEYS = {
     WEATHER_CACHE_HITS: 'weatherCacheHits',
     WEATHER_CACHE_MISSES: 'weatherCacheMisses',
+
+    HTTP_REQUESTS_TOTAL: 'http_requests_total',
+    HTTP_REQUEST_DURATION: 'http_request_duration_seconds',
+    HTTP_REQUEST_ERRORS: 'http_request_errors_total',
+
+    APP_UPTIME: 'app_uptime_seconds',
 };
 
 module.exports = METRIC_KEYS;
